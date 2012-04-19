38a44d3479f3502d7230ec8d8f6c37c1662a04ed
David Kurokawa <DavidKurokawa@gmail.com>
2012-04-19 16:56:26 -0700

    Forgot to add base_graph files.

---
+++ prpack_base_graph.h
@@ -0,0 +1,31 @@
+#ifndef PRPACK_ADJACENCY_LIST
+#define PRPACK_ADJACENCY_LIST
+#include "prpack_csr.h"
+#include "prpack_edge_list.h"
+#include <cstdio>
+#include <string>
+
+namespace prpack {
+
+	class prpack_base_graph {
+		private:
+			// helper methods
+			void read_smat(std::FILE* f);
+			void read_edges(std::FILE* f);
+			void read_ascii(std::FILE* f);
+		public:
+			// instance variables
+			int num_vs;
+			int num_es;
+			int num_self_es;
+			int* heads;
+			int* tails;
+			// constructors
+			prpack_base_graph(prpack_csr* g);
+			prpack_base_graph(prpack_edge_list* g);
+			prpack_base_graph(const std::string& filename, const std::string& format);
+	};
+
+};
+
+#endif
