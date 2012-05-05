772b065b9b87ccadb64c0d37ede8e9b174ddcca0
David Kurokawa <DavidKurokawa@gmail.com>
2012-05-04 22:14:48 -0700

    Change time so that it works on windows.

--- prpack_utils.cpp
+++ prpack_utils.cpp
@@ -1,6 +1,6 @@
 #include "prpack_utils.h"
-#include <sys/time.h>
 #include <cassert>
+#include <ctime>
 #include <iostream>
 #include <string>
 using namespace prpack;
@@ -8,9 +8,7 @@ using namespace std;
 
 // Returns the current time.
 double prpack_utils::get_time() {
-	struct timeval t;
-	gettimeofday(&t, 0);
-	return t.tv_sec + t.tv_usec/double(1.0e6);
+    return clock()/double(CLOCKS_PER_SEC);
 }
 
 // Fails and outputs 'msg' if 'condition' is false.
