06de84d7ce2e50d89374d333795a20796e4a34c4
David Gleich <dgleich@purdue.edu>
2012-05-09 17:53:13 -0400

    Fixed to use 0 indices

--- matlab/pagerank.m
+++ matlab/pagerank.m
@@ -3,9 +3,9 @@ function [x,ret] = pagerank(A)
 assert(size(A,1)==size(A,2));
 n=size(A,1);
 [i,j,v] = find(A);
-i = int32(i);
-j = int32(j);
+i = int32(i-1);
+j = int32(j-1);
 S = pagerank_solver(int32(n),i,j);
-u = ones(n,1)./n;
-v = ones(n,1)./n;
+u = [];
+v = [];
 [x,ret] = S.solve(0.85,1e-10,u,v,'gs');
