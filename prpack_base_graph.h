7c891cbff55da7923a2827de3bd833bd36bdc27b
David Gleich <dgleich@purdue.edu>
2013-06-12 19:53:52 -0400

    Start of switch to prpack_vtype instead of int

--- prpack_base_graph.h
+++ prpack_base_graph.h
@@ -8,6 +8,25 @@
 
 namespace prpack {
 
+    typedef int prpack_vtype;
+
+    /** Convert any input graph into something prpack can process itself.
+     * 
+     * prpack_base_graph is an in-links based representation of a graph.
+     * tails[i]:tails[i+1] is an index into the heads array for all
+     * edges that end at vertex i.
+     *
+     * for (int i=0; i < num_vs; ++i) {
+     *   int end = i==num_vs-1 ? num_es : tails[i+1]; // handle end
+     *   for (int j=tails[i]; j < end; ++j) {
+     *       // there is a directed edge from heads[j] -> i
+     *    }
+     *  }
+     *
+     * Thus the graph is an in-edge adjacency list. 
+     *
+     */
+    
     class prpack_base_graph {
         private:
             // helper methods
@@ -17,11 +36,11 @@ namespace prpack {
             void read_ascii(std::FILE* f);
         public:
             // instance variables
-            int num_vs;
-            int num_es;
-            int num_self_es;
-            int* heads;
-            int* tails;
+            prpack_vtype num_vs;
+            prpack_vtype num_es;
+            prpack_vtype num_self_es;
+            prpack_vtype* heads;
+            prpack_vtype* tails;
             double* vals;
             // constructors
             prpack_base_graph();    // only to support inheritance
@@ -30,7 +49,8 @@ namespace prpack {
             prpack_base_graph(const prpack_csr* g);
             prpack_base_graph(const prpack_edge_list* g);
             prpack_base_graph(const char* filename, const char* format, const bool weighted);
-            prpack_base_graph(int nverts, int nedges, std::pair<int,int>* edges);
+            prpack_base_graph(prpack_vtype nverts, prpack_vtype nedges, 
+                std::pair<prpack_vtype,prpack_vtype>* edges);
             // destructor
             ~prpack_base_graph();
             // operations
